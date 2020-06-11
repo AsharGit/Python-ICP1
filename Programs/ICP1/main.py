7cba07be7bc236ff701ac0813ad8a9ad8ae02a32
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-06-11 00:43:25 -0500

    Update main.py

--- Programs/ICP1/main.py
+++ Programs/ICP1/main.py
@@ -1,3 +1,10 @@
+# Question 1
+# There were several changes between python 2 and 3 version
+# One difference was the print command was changed to print() function.
+# Ex: print 'hello world' would work in V2 but not in V3.
+# Another is that by default the output produced by integer division is a float in version 3.
+# Ex: 3/2 = 1 in V2 while 3/2 = 1.5 in V3.
+
 
 # Question 2
 ilist = []
@@ -20,3 +27,18 @@ del ilist[3]
 ilist.reverse()
 print(*ilist, sep="")
 
+
+# Question 3
+# Get input sentence from user and put in list
+print("Input sentence:")
+sentence = input()
+jlist = sentence.split()
+
+print(*jlist, sep=" ")
+
+# Search and replace "python" with "pythons"
+for i in range(len(jlist)):
+    if jlist[i] == "python":
+        jlist[i] = "pythons"
+
+print(*jlist, sep=" ")
